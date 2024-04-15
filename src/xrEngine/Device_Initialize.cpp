097e68197f6146f1314165b7bccab0b93cf697c3
MagilaWEB <78775512+MagilaWEB@users.noreply.github.com>
2024-04-15 23:01:38 +0300

    Update 1.0.0.1 (15.04.2024)

--- src/xrEngine/Device_Initialize.cpp
+++ src/xrEngine/Device_Initialize.cpp
@@ -11,74 +11,74 @@ extern LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lPar
 
 void CRenderDevice::initialize_weather_editor()
 {
-    m_editor_module = XRay::LoadModule("xrWeatherEditor");
-    if (!m_editor_module->IsLoaded())
-        return;
+	m_editor_module = XRay::LoadModule("xrWeatherEditor");
+	if (!m_editor_module->IsLoaded())
+		return;
 
-    m_editor_initialize = (initialize_function_ptr)m_editor_module->GetProcAddress("initialize");
-    VERIFY(m_editor_initialize);
+	m_editor_initialize = (initialize_function_ptr)m_editor_module->GetProcAddress("initialize");
+	VERIFY(m_editor_initialize);
 
-    m_editor_finalize = (finalize_function_ptr)m_editor_module->GetProcAddress("finalize");
-    VERIFY(m_editor_finalize);
+	m_editor_finalize = (finalize_function_ptr)m_editor_module->GetProcAddress("finalize");
+	VERIFY(m_editor_finalize);
 
-    m_engine = new engine_impl();
-    m_editor_initialize(m_editor, m_engine);
-    VERIFY(m_editor);
+	m_engine = new engine_impl();
+	m_editor_initialize(m_editor, m_engine);
+	VERIFY(m_editor);
 
-    m_hWnd = m_editor->view_handle();
-    VERIFY(m_hWnd != INVALID_HANDLE_VALUE);
+	m_hWnd = m_editor->view_handle();
+	VERIFY(m_hWnd != INVALID_HANDLE_VALUE);
 
-    GEnv.isEditor = true;
+	GEnv.isEditor = true;
 }
 
 void CRenderDevice::Initialize()
 {
-    Log("Initializing Engine...");
-    TimerGlobal.Start();
-    TimerMM.Start();
+	Log("Initializing Engine...");
+	TimerGlobal.Start();
+	TimerMM.Start();
 
-    if (strstr(Core.Params, "-weather"))
-        initialize_weather_editor();
+	if (strstr(Core.Params, "-weather"))
+		initialize_weather_editor();
 
-    // Unless a substitute hWnd has been specified, create a window to render into
-    if ( m_hWnd == NULL)
-    {
-        const char* wndclass = "_XRAY_1.6";
-        // Register the windows class
-        HINSTANCE hInstance = (HINSTANCE)GetModuleHandle(0);
-        WNDCLASS wndClass = {0, WndProc, 0, 0, hInstance, LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON1)),
-            LoadCursor(NULL, IDC_ARROW), (HBRUSH)GetStockObject(BLACK_BRUSH), NULL, wndclass};
-        RegisterClass(&wndClass);
-        // Set the window's initial style
-        m_dwWindowStyle = WS_BORDER | WS_DLGFRAME;
-        // Set the window's initial width
-        u32 screen_width = GetSystemMetrics(SM_CXSCREEN);
-        u32 screen_height = GetSystemMetrics(SM_CYSCREEN);
+	// Unless a substitute hWnd has been specified, create a window to render into
+	if (m_hWnd == NULL)
+	{
+		const char* wndclass = "_XRAY_1.6";
+		// Register the windows class
+		HINSTANCE hInstance = (HINSTANCE)GetModuleHandle(0);
+		WNDCLASS wndClass = { 0, WndProc, 0, 0, hInstance, LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON1)),
+			LoadCursor(NULL, IDC_ARROW), (HBRUSH)GetStockObject(BLACK_BRUSH), NULL, wndclass };
+		RegisterClass(&wndClass);
+		// Set the window's initial style
+		m_dwWindowStyle = WS_BORDER | WS_DLGFRAME;
+		// Set the window's initial width
+		u32 screen_width = GetSystemMetrics(SM_CXSCREEN);
+		u32 screen_height = GetSystemMetrics(SM_CYSCREEN);
 
-        DEVMODE screen_settings;
-        memset(&screen_settings, 0, sizeof(screen_settings));
-        screen_settings.dmSize = sizeof(screen_settings);
-        screen_settings.dmPelsWidth = (unsigned long)screen_width;
-        screen_settings.dmPelsHeight = (unsigned long)screen_height;
-        screen_settings.dmBitsPerPel = 32;
-        screen_settings.dmFields = DM_BITSPERPEL | DM_PELSWIDTH | DM_PELSHEIGHT;
+		DEVMODE screen_settings;
+		memset(&screen_settings, 0, sizeof(screen_settings));
+		screen_settings.dmSize = sizeof(screen_settings);
+		screen_settings.dmPelsWidth = (unsigned long)screen_width;
+		screen_settings.dmPelsHeight = (unsigned long)screen_height;
+		screen_settings.dmBitsPerPel = 32;
+		screen_settings.dmFields = DM_BITSPERPEL | DM_PELSWIDTH | DM_PELSHEIGHT;
 
-        ChangeDisplaySettings(&screen_settings, CDS_FULLSCREEN);
+		ChangeDisplaySettings(&screen_settings, CDS_FULLSCREEN);
 
-        // Create the render window
-        m_hWnd = CreateWindowEx(WS_EX_TOPMOST, wndclass, "S.T.A.L.K.E.R.: Call of Chernobyl", m_dwWindowStyle, 0, 0, screen_width, screen_height, 0L, 0, hInstance, 0L );
-    }
-    // Save window properties
-    m_dwWindowStyle = GetWindowLong(m_hWnd, GWL_STYLE);
-    GetWindowRect(m_hWnd, &m_rcWindowBounds);
-    GetClientRect(m_hWnd, &m_rcWindowClient);
+		// Create the render window
+		m_hWnd = CreateWindowEx(WS_EX_TOPMOST, wndclass, make_string("%s: version %s", PRODUCT_NAME, PRODUCT_VIRSION).c_str(), m_dwWindowStyle, 0, 0, screen_width, screen_height, 0L, 0, hInstance, 0L);
+	}
+	// Save window properties
+	m_dwWindowStyle = GetWindowLong(m_hWnd, GWL_STYLE);
+	GetWindowRect(m_hWnd, &m_rcWindowBounds);
+	GetClientRect(m_hWnd, &m_rcWindowClient);
 }
 
 void CRenderDevice::DumpStatistics(IGameFont& font, IPerformanceAlert* alert)
 {
-    font.OutNext("*** ENGINE:   %2.2fms", stats.EngineTotal.result);
-    font.OutNext("FPS/RFPS:     %3.1f/%3.1f", stats.fFPS, stats.fRFPS);
-    font.OutNext("TPS:          %2.2f M", stats.fTPS);
-    if (alert && stats.fFPS < 30)
-        alert->Print(font, "FPS       < 30:   %3.1f", stats.fFPS);
+	font.OutNext("*** ENGINE:   %2.2fms", stats.EngineTotal.result);
+	font.OutNext("FPS/RFPS:     %3.1f/%3.1f", stats.fFPS, stats.fRFPS);
+	font.OutNext("TPS:          %2.2f M", stats.fTPS);
+	if (alert && stats.fFPS < 30)
+		alert->Print(font, "FPS       < 30:   %3.1f", stats.fFPS);
 }
