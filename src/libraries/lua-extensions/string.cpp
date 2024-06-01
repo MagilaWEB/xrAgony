e0301fa8dacc2175d1383900d53232b66918d7bc
MagilaWEB <78775512+MagilaWEB@users.noreply.github.com>
2024-06-01 10:09:14 +0300

    -is_contain для строковых объектов lua на C.

--- src/libraries/lua-extensions/string.cpp
+++ src/libraries/lua-extensions/string.cpp
@@ -1,18 +1,18 @@
 #include "lua.hpp"
-#include "string.h"
 #include <cstdlib>
 #include <cctype>
+#include <string.h>
 
 void set_info_extend_string(lua_State* L)
 {
 	lua_pushliteral(L, "EXTENDED_STRING_COPYRIGHT");
-	lua_pushliteral(L, "Copyright (C) A.R.E.A 2017-2023.");
+	lua_pushliteral(L, "Copyright (C) A.R.E.A 2017-2024.");
 	lua_settable(L, -3);
 	lua_pushliteral(L, "EXTENDED_STRING_DESCRIPTION");
 	lua_pushliteral(L, "EXTENDED_STRING is a Lua library that extends and adds new methods for working with string objects luajit v2.1.");
 	lua_settable(L, -3);
 	lua_pushliteral(L, "EXTENDED_STRING_VERSION");
-	lua_pushliteral(L, "EXTENDED_STRING v0.1.1 (21.11.2023)");
+	lua_pushliteral(L, "EXTENDED_STRING v0.1.2 (01.06.2024)");
 	lua_settable(L, -3);
 }
 
@@ -65,7 +65,7 @@ int str_trim_w(lua_State* L)
 {
 	luaL_checktype(L, 1, LUA_TSTRING);
 	int i = 0, d, n;
-	const char* s = luaL_checkstring(L, 1);;
+	const char* s = luaL_checkstring(L, 1);
 	while (s[i] == ' ') i++;
 	n = i;
 	while (s[i] != ' ' && s[i]) i++;
@@ -74,11 +74,24 @@ int str_trim_w(lua_State* L)
 	return 1;
 }
 
+int is_contain(lua_State* L)
+{
+	luaL_checktype(L, 1, LUA_TSTRING);
+	luaL_checktype(L, 2, LUA_TSTRING);
+
+	const char* string_1 = luaL_checkstring(L, 1);
+	const char* string_2 = luaL_checkstring(L, 2);
+
+	lua_pushboolean(L, strstr(string_1, string_2) != NULL);
+	return 1;
+}
+
 const struct luaL_Reg strlib[] = {
 	{"trim", str_trim},
 	{"trim_l", str_trim_l},
 	{"trim_r", str_trim_r},
 	{"trim_w", str_trim_w},
+	{"is_contain", is_contain},
 	{NULL, NULL}
 };
 
