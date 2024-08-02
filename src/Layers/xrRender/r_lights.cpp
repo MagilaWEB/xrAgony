f6d0399623bec59781c9a99cf13aee0fbfdef85d
MagilaWEB <78775512+MagilaWEB@users.noreply.github.com>
2024-08-03 00:47:21 +0300

    -Исправлен баг с легким отбраковыванием.

--- src/Layers/xrRender/r_lights.cpp
+++ src/Layers/xrRender/r_lights.cpp
@@ -85,9 +85,10 @@ void CRender::render_lights(light_Package& LP)
 
 			r_pmask(true, !!RImplementation.o.Tshadows);
 
-			L->svis.begin();
 			PIX_EVENT(SHADOWED_LIGHTS_RENDER_SUBSPACE);
 			r_dsgraph_render_subspace(L->spatial.sector, L->X.S.combine, L->position, TRUE);
+			L->svis.begin();
+
 			bool bNormal = mapNormalPasses[0][0].size() || mapMatrixPasses[0][0].size();
 			bool bSpecial = mapNormalPasses[1][0].size() || mapMatrixPasses[1][0].size() || mapSorted.size();
 			if (bNormal || bSpecial)
